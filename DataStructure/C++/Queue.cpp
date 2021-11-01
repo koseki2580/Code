4eca78f717e799899546e49db432cdf66ecfafbe
koseki2580 <kingdom_hearts2580@yahoo.co.jp>
2021-11-01 22:07:35 +0900

    2021/11/1

--- DataStructure/C++/Queue.cpp
+++ DataStructure/C++/Queue.cpp
@@ -10,12 +10,17 @@ using P = pair<int,int>;
 
 int main() {
 
+    // 初期化
     queue<int> s;
+    // push　要素を後ろに追加
     s.push(1);
     s.push(2);
     s.push(3);
+    // 一番前の要素を確認
     cout << s.front() << endl; // 1  s:[1,2,3]
     s.push(4);
+
+    // pop　一番前の要素を取り除く
     s.pop();
     cout << s.front() << endl; // 2  s:[2,3,4]
     s.pop();
--- DataStructure/C++/Stack.cpp
+++ DataStructure/C++/Stack.cpp
@@ -10,13 +10,18 @@ using P = pair<int,int>;
 
 int main() {
 
+    // 初期化
     stack<int> s;
+    // push　要素を後ろに追加
     s.push(1);
     s.push(2);
     s.push(3);
+    // 一番後ろの要素を確認
     cout << s.top() << endl; // 3  s:[1,2,3]
     s.push(4);
     cout << s.top() << endl; // 4  s:[1,2,3,4]
+
+    // pop 一番後ろ（一番新しい）要素を取り除く
     s.pop();
     s.pop();
     cout << s.top() << endl; // 2  s:[1,2]
