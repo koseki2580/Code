98bbb13a45bf5a2d2eea9aa2c122f753b0ad0c84
koseki2580 <kingdom_hearts2580@yahoo.co.jp>
2022-05-21 19:20:59 +0900

    2022/5/21

---
+++ Redcoder/C++/77.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#define rep(i,nn,n) for (int i = nn; i < (n); ++i)
+#define rrep(i,nn,n) for (int i = nn; i > (n); --i)
+using namespace std;
+using ll = long long;
+using P = pair<int,int>;
+#define chmin(x,y) x = min(x,y)
+#define chmax(x,y) x = max(x,y)
+
+const int mod = 100000;
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x=0):x((x%mod+mod)%mod){}
+    mint operator-() const { return mint(-x);}
+    mint& operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator-=(const mint a) {
+        if ((x += mod-a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+    mint operator+(const mint a) const { return mint(*this) += a;}
+    mint operator-(const mint a) const { return mint(*this) -= a;}
+    mint operator*(const mint a) const { return mint(*this) *= a;}
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+        if (t&1) a *= *this;
+        return a;
+    }
+    // for prime mod
+    mint inv() const { return pow(mod-2);}
+    mint& operator/=(const mint a) { return *this *= a.inv();}
+    mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+int main() {
+    int N,M;
+    cin >> N >> M;
+    vector<int> inn(N+1,0);
+    rep(i,2,N+1){
+        int a;
+        cin >> a;
+        inn[i] = inn[i-1] + a;
+    }
+    mint ans = 0;
+    int s = 1;
+    rep(i,0,M){
+        int next;
+        cin >> next;
+        int mi,ma;
+        ma = max(s,s+next);
+        mi = min(s,s+next);
+        ans += inn[ma] - inn[mi];
+        s += next;
+    }
+
+    cout << ans.x << endl;
+    return 0;
+}
