7db0543cb036b90e6f6c068183ea2b9af41ff9fc
koseki2580 <kingdom_hearts2580@yahoo.co.jp>
2022-06-07 16:29:10 +0900

    2022/6/7

---
+++ Typical_90/Python/61.py
@@ -0,0 +1,11 @@
+from collections import deque
+Q = int(input())
+q = deque()
+for i in range(Q):
+    t, x = map(int, input().split())
+    if t == 1:
+        q.appendleft(x)
+    elif t == 2:
+        q.append(x)
+    else:
+        print(q[x-1])
