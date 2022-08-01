702504f7b160f244fe95ad8025b2687749fb1d82
koseki2580 <kingdom_hearts2580@yahoo.co.jp>
2022-08-01 16:49:35 +0900

    2022/8/1

---
+++ Redcoder/C++/98.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#define rep(i,nn,n) for (int i = nn; i < (n); ++i)
+#define rrep(i,nn,n) for (int i = nn; i > (n); --i)
+using namespace std;
+using ll = long long;
+using P = pair<int,int>;
+#define chmin(x,y) x = min(x,y)
+#define chmax(x,y) x = max(x,y)
+
+const int mod = 1000000007;
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x=0):x((x%mod+mod)%mod){}
+    mint operator-() const { return mint(-x);}
+    mint& operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator-=(const mint a) {
+        if ((x += mod-a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+    mint operator+(const mint a) const { return mint(*this) += a;}
+    mint operator-(const mint a) const { return mint(*this) -= a;}
+    mint operator*(const mint a) const { return mint(*this) *= a;}
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+        if (t&1) a *= *this;
+        return a;
+    }
+    // for prime mod
+    mint inv() const { return pow(mod-2);}
+    mint& operator/=(const mint a) { return *this *= a.inv();}
+    mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+int main() {
+    int N ;
+    cin >> N;
+    vector<int> A(N);
+    rep(i,0,N) {
+        cin >> A[i];
+    }
+    mint ans = 1;
+    vector<int> cnt(N+1,0);
+    cnt[0] = 3;
+    rep(i,0,N){
+        int num = A[i];
+        ++num;
+        ans *= (cnt[num-1] - cnt[num]);
+        ++cnt[num];
+    }
+    cout << ans << endl;
+    return 0;
+}
